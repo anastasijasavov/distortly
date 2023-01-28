8f14ac76d28a1368914fd1aebe6f7af52c609490
Anastasija <anastasija.savov2000@gmail.com>
2023-01-28 21:43:20 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'test.ionic.app',
+  appName: 'test-ionic-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
